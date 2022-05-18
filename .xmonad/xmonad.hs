3a6098b06c4f1121ef165b8e6a1a11c1de884735
Roman <ardazishvili.roman@yandex.ru>
2022-05-18 23:51:05 +0300

    Add polybar workspaces support (#1)

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -9,7 +9,22 @@ import XMonad.Layout.DwmStyle
 -- import XMonad.Hooks.FadeInactive
 import XMonad.Hooks.FadeWindows
 
-main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
+-- For polybar
+import qualified DBus as D
+import qualified DBus.Client as D
+import qualified Codec.Binary.UTF8.String as UTF8
+
+main = do 
+  dbus <- D.connectSession
+  -- Request access to the DBus name
+  D.requestName dbus (D.busName_ "org.xmonad.Log") [D.nameAllowReplacement, D.nameReplaceExisting, D.nameDoNotQueue]
+  xmonad $ def {
+      terminal = "alacritty"
+      , layoutHook = baseLayout
+      , manageHook = manageDocks
+      , logHook = dynamicLogWithPP (myLogHook dbus)
+      , startupHook = spawn "compton --config ~/.xmonad/compton.conf" <+> spawn "setxkbmap -layout us,ru -option 'grp:lctrl_lshift_toggle'"
+    } `additionalKeys` myKeys
 
 myBar = "xmobar"
 myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
@@ -33,16 +48,51 @@ mySDConfig = def {
                  }
 
 myStartupHook :: X ()
-myStartupHook = spawn "compton --config ~/.xmonad/compton.conf"
+myStartupHook = do
+    spawn "compton --config ~/.xmonad/compton.conf"
 
-myConfig = def {
-      terminal = "alacritty"
-      , layoutHook = baseLayout
-      , manageHook = manageDocks
-      , startupHook = spawn "compton --config ~/.xmonad/compton.conf" <+> spawn "setxkbmap -layout us,ru -option 'grp:lctrl_lshift_toggle'" 
-    } `additionalKeys` myKeys
+myLogHook :: D.Client -> PP
+myLogHook dbus = def
+    { ppOutput = dbusOutput dbus
+    , ppCurrent = wrap ("%{B" ++ bg2 ++ "} ") " %{B-}"
+    , ppVisible = wrap ("%{B" ++ bg1 ++ "} ") " %{B-}"
+    , ppUrgent = wrap ("%{F" ++ red ++ "} ") " %{F-}"
+    , ppHidden = wrap " " " "
+    , ppWsSep = ""
+    , ppSep = " : "
+    , ppTitle = shorten 40
+    }
 
-baseLayout = noBorders $ avoidStruts $ Full ||| tiled 
+dbusOutput :: D.Client -> String -> IO ()
+dbusOutput dbus str = do
+    let signal = (D.signal objectPath interfaceName memberName) {
+            D.signalBody = [D.toVariant $ UTF8.decodeString str]
+        }
+    D.emit dbus signal
   where
-    tiled =  spacingRaw False (Border 10 0 10 0) True (Border 0 10 0 10) True $ Tall 1 (3/100) (1/2) 
+    objectPath = D.objectPath_ "/org/xmonad/Log"
+    interfaceName = D.interfaceName_ "org.xmonad.Log"
+    memberName = D.memberName_ "Update"
 
+
+baseLayout = noBorders $ avoidStruts $ full ||| tiled 
+  where
+    tiled =  spacingRaw False (Border 0 35 10 0) True (Border 0 10 10 0) True $ Tall 1 (3/100) (1/2) 
+    full =  spacingRaw False (Border 0 45 10 0) True (Border 0 0 0 0) True $ Full
+
+fg        = "#ebdbb2"
+bg        = "#282828"
+gray      = "#a89984"
+bg1       = "#3c3836"
+bg2       = "#504945"
+bg3       = "#665c54"
+bg4       = "#7c6f64"
+
+green     = "#b8bb26"
+darkgreen = "#98971a"
+red       = "#fb4934"
+darkred   = "#cc241d"
+yellow    = "#fabd2f"
+blue      = "#83a598"
+purple    = "#d3869b"
+aqua      = "#8ec07c"
